0f753c61e76d0decddf25793cb88d6cffa060f09
Kayla Dailey <kayladailey000@gmail.com>
2020-02-03 17:08:24 -0600

    Completed step 1

---
+++ todo/src/reducers/ReducerTodo.js
@@ -0,0 +1,13 @@
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  };
+
+const ReducerTodo = (todo) => {
+  const newTodo =  [...todo, newTodo]
+  return newTodo;
+}
+
+export default ReducerTodo;
